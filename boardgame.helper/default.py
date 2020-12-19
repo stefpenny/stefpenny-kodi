6814245e05221fb2601e84831b76ae635643c71c
stefpenny <stephane.delogu@gmail.com>
2020-12-19 14:29:16 +0100

    1.0.9

--- boardgame.helper/default.py
+++ boardgame.helper/default.py
@@ -8,20 +8,22 @@ import time
 import sys
 import imagedownload
 import collection
+import os
 
 try:
-   import StorageServer
+    import StorageServer
 except:
-   import storageserverdummy as StorageServer
+    import storageserverdummy as StorageServer
 
-cache = StorageServer.StorageServer("boardgame.helper", 24) 
+cache = StorageServer.StorageServer("boardgame.helper", 24)
 
 # create a class for your addon, we need this to get info about your addon
 ADDON = xbmcaddon.Addon()
 # get the full path to your addon, decode it to unicode to handle special (non-ascii) characters in the path
-#CWD = ADDON.getAddonInfo('path') # for kodi 19 and up..
+# CWD = ADDON.getAddonInfo('path') # for kodi 19 and up..
 CWD = ADDON.getAddonInfo('path').decode('utf-8')
-__profile__ = xbmc.translatePath( ADDON.getAddonInfo('profile') ).decode("utf-8")
+__profile__ = xbmc.translatePath(ADDON.getAddonInfo('profile')).decode("utf-8")
+
 
 # add a class to create your xml based window
 class GUI(xbmcgui.WindowXML):
@@ -32,13 +34,19 @@ class GUI(xbmcgui.WindowXML):
         self.table = []
         self.curFilter = 0
         self.curSort = 0
+        self.mediapath = os.path.join(CWD, 'resources', 'skins', 'Default', 'media')
 
     def onClick(self, control):
-        if control == 3001:
+        if control == 50:
+            curctl = self.getFocus()
+            listitem = curctl.getListItem(1)
+
+        if control == self.SortButton.getId():
             dialog = xbmcgui.Dialog()
-            entries = ["By Name (ASC)", "By Name (DESC)", "By Year published (ASC)", "By Year published (DESC)", "By Type (ASC)", "By Type (DESC)"]
+            entries = ["By Name (ASC)", "By Name (DESC)", "By Year published (ASC)", "By Year published (DESC)",
+                       "By Type (ASC)", "By Type (DESC)"]
             nr = dialog.select("Sort Games", entries)
-            if nr>=0:
+            if nr >= 0:
                 entry = entries[nr]
                 if entry == "By Name (ASC)":
                     self.curSort = 0
@@ -52,16 +60,16 @@ class GUI(xbmcgui.WindowXML):
                     self.curSort = 4
                 elif entry == "By Type (DESC)":
                     self.curSort = 5
-                    
-                self.clearList()                
+
+                self.clearList()
                 listitems = self.build_list(self.curFilter, self.curSort)
                 self.addItems(listitems)
 
-        if control == 3002:
+        if control == self.FilterButton.getId():
             dialog = xbmcgui.Dialog()
             entries = ["Show All Owned", "Show Base Games", "Show Expansions", "Show WishList"]
             nr = dialog.select("Filter Games", entries)
-            if nr>=0:
+            if nr >= 0:
                 entry = entries[nr]
                 if entry == "Show All Owned":
                     self.curFilter = 0
@@ -97,27 +105,27 @@ class GUI(xbmcgui.WindowXML):
                 full_list.append(elt)
 
         if sort == 0:
-            full_list.sort(key=lambda x: x.get('name'))        
+            full_list.sort(key=lambda x: x.get('name'))
         elif sort == 1:
-            full_list.sort(key=lambda x: x.get('name'), reverse=True)        
+            full_list.sort(key=lambda x: x.get('name'), reverse=True)
         elif sort == 2:
-            full_list.sort(key=lambda x: x.get('year_published'))        
+            full_list.sort(key=lambda x: x.get('year_published'))
         elif sort == 3:
-            full_list.sort(key=lambda x: x.get('year_published'), reverse=True)        
+            full_list.sort(key=lambda x: x.get('year_published'), reverse=True)
         elif sort == 4:
-            full_list.sort(key=lambda x: x.get('subtype'))        
+            full_list.sort(key=lambda x: x.get('subtype'))
         elif sort == 5:
-            full_list.sort(key=lambda x: x.get('subtype'), reverse=True)        
-
+            full_list.sort(key=lambda x: x.get('subtype'), reverse=True)
 
         for elt in full_list:
             file_name = elt['thumbnail'].cdata.split('/')[-1]
             file_ext = file_name.split('.')[-1]
 
             listitem = xbmcgui.ListItem(elt['name'] + ' (' + str(elt['year_published']) + ')')
-            listitem.setArt({ 'icon': str(__profile__ + 'imgCache\\' + str(elt['bgg_id'] + '.' + file_ext))  })
+            listitem.setIsFolder(True)
+            listitem.setArt({'icon': str(__profile__ + 'imgCache\\' + str(elt['bgg_id'] + '.' + file_ext))})
 
-            #Item properties
+            # Item properties
             listitem.setProperty('RatingValue', str(elt['rating']))
 
             RankValue = 'RANK: '
@@ -151,11 +159,12 @@ class GUI(xbmcgui.WindowXML):
             listitem.setProperty('NumPlayers', NumPlayers)
             listitem.setProperty('PlayingTime', PlayingTime)
 
-            listitems.append(listitem)   
+            listitems.append(listitem)
 
-        return listitems 
+        return listitems
 
-    # until now we have a blank window, the onInit function will parse your xml file
+        # until now we have a blank window, the onInit function will parse your xml file
+
     def onInit(self):
 
         self.setProperty('WinWidth', str(self.getWidth()))
@@ -164,7 +173,7 @@ class GUI(xbmcgui.WindowXML):
         self.setProperty('TypeIcon', 'genius-icon.png')
         self.setProperty('LineSepV', 'linev.png')
         self.setProperty('LineSepH', 'lineh.png')
-        self.setProperty('TestValue', 'Hello')
+        self.setProperty('TestValue', __profile__)
 
         xbmc.executebuiltin('Container.SetViewMode(50)')
         my_addon = xbmcaddon.Addon()
@@ -176,48 +185,56 @@ class GUI(xbmcgui.WindowXML):
 
         cache.table_name = "BGGInfos"
 
-        #table = cache.get("collection")
-        #if not table:            
-        
+        # table = cache.get("collection")
+        # if not table:
+
         self.table = collection.Collection(user_name)
         self.table.load()
 
-        #cache.set("collection", table)
+        # cache.set("collection", table)
 
-        self.setProperty('CollectionStats', user_name + '\'s collection: ' + str(self.table.total_owned) + ' Boardgames / ' + str(self.table.total_exp) + ' Expansions')
+        self.setProperty('CollectionStats',
+                         user_name + '\'s collection: ' + str(self.table.total_owned) + ' Boardgames / ' + str(
+                             self.table.total_exp) + ' Expansions')
 
         i = 1
         for elt in self.table.games:
-            
+
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['image'].cdata), str(elt['bgg_id']))
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['thumbnail'].cdata), str(elt['bgg_id']) + '_tn')
 
-            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list), line1 = 'Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list), line2 = elt['name'])
+            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list),
+                            line1='Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list),
+                            line2=elt['name'])
             i = i + 1
 
-            if (progress.iscanceled()):
+            if progress.iscanceled():
                 break
 
         for elt in self.table.expansions:
-            
+
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['image'].cdata), str(elt['bgg_id']))
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['thumbnail'].cdata), str(elt['bgg_id']) + '_tn')
 
-            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list), line1 = 'Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list), line2 = elt['name'])
+            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list),
+                            line1='Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list),
+                            line2=elt['name'])
             i = i + 1
 
-            if (progress.iscanceled()):
+            if progress.iscanceled():
                 break
 
         for elt in self.table.wish_list:
-            
+
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['image'].cdata), str(elt['bgg_id']))
             imagedownload.download(__profile__ + 'imgCache\\', str(elt['thumbnail'].cdata), str(elt['bgg_id']) + '_tn')
 
-            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list), line1 = 'Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list), line2 = elt['name'])
+            progress.update(100 * i / (self.table.total_owned + self.table.total_wish_list),
+                            line1='Game ' + str(i) + ' of ' + str(self.table.total_owned + self.table.total_wish_list),
+                            line2=elt['name'])
             i = i + 1
 
-            if (progress.iscanceled()):
+            if progress.iscanceled():
                 break
 
         progress.update(100)
@@ -225,40 +242,37 @@ class GUI(xbmcgui.WindowXML):
 
         listitems = self.build_list(self.curFilter, self.curSort)
 
-        # by default the built-in container already contains one item, the 'up' (..) item, let's remove that one
         self.clearList()
-        # now we are going to add all the items we have defined to the (built-in) container
         self.addItems(listitems)
 
-        self.button0 = xbmcgui.ControlButton(1500, 5, 270, 30, 'Sort Games')
-        self.addControl(self.button0)
+        self.SortButton = xbmcgui.ControlButton(1500, 10, 270, 50, 'Sort Games', alignment=2
+                                                , focusTexture=os.path.join(self.mediapath, 'buttonhover.png')
+                                                , noFocusTexture=os.path.join(self.mediapath, 'button.png'))
+        self.addControl(self.SortButton)
 
-        self.button1 = xbmcgui.ControlButton(1000, 5, 270, 30, 'Filter Games')
-        self.addControl(self.button1)
+        self.FilterButton = xbmcgui.ControlButton(1200, 10, 270, 50, 'Filter Games', alignment=2
+                                                , focusTexture=os.path.join(self.mediapath, 'buttonhover.png')
+                                                , noFocusTexture=os.path.join(self.mediapath, 'button.png'))
+        self.addControl(self.FilterButton)
 
-        # give kodi a bit of (processing) time to add all items to the container
+        # Navigation between controls
+        self.SortButton.controlRight(self.FilterButton)
+        self.SortButton.controlLeft(self.FilterButton)
+        self.FilterButton.controlRight(self.SortButton)
+        self.FilterButton.controlLeft(self.SortButton)
+
+        self.SortButton.controlDown(self.getControl(50))
+        self.FilterButton.controlDown(self.getControl(50))
+
+        self.getControl(50).controlLeft(self.FilterButton)
+        self.getControl(50).controlRight(self.SortButton)
+
         xbmc.sleep(100)
         # this puts the focus on the top item of the container
         self.setFocusId(self.getCurrentContainerId())
-        
 
-# this is the entry point of your addon, execution of your script will start here
-if (__name__ == '__main__'):
-    # define your xml window and pass these four (kodi 17) or five (kodi 18) arguments (more optional items can be passed as well):
-    # 1 'the name of the xml file for this window', 
-    # 2 'the path to your addon',
-    # 3 'the name of the folder that contains the skin',
-    # 4 'the name of the folder that contains the skin xml files'
-    # 5 [kodi 18] set to True for a media window (a window that will list music / videos / pictures), set to False otherwise
-    # 6 [optional] if you need to pass additional data to your window, simply add them to the list
-    # you'll have to add them as key=value pairs: key1=value1, key2=value2, etc...
-    ui = GUI('script-testwindow.xml', CWD, 'default', '1080i', True, optional1='some data') # for kodi 18 and up..
-#    ui = GUI('script-testwindow.xml', CWD, 'default', '1080i', optional1='some data') # use this line instead for kodi 17 and earlier
-    # now open your window. the window will be shown until you close your addon
+
+if __name__ == '__main__':
+    ui = GUI('bgglist.xml', CWD, 'default', '1080i', True, optional1='some data')
     ui.doModal()
-    # window closed, now cleanup a bit: delete your window before the script fully exits
     del ui
-
-# the end!
-
-
